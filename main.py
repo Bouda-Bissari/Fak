3c00b4207964aeec5cea45fc1d0dd62b0fefc46c
Bouda-Bissari <jeanmariebissari@gmail.com>
2023-12-21 00:48:45 +0000

    onother commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fonctions import est_pair, jeu_devinette_nombre
 # Test de la fonction est_pair
 nombre_test = 17
 print(f"Le nombre {nombre_test} est {'pair' if est_pair(nombre_test) else 'impair'}.")
+print("hi")
 
 # Lancer le jeu de devinette de nombre
 jeu_devinette_nombre()
